33154359db35e4239a654c48e1720bb2c182d28b
Adam Sidor <adamsidor94@gmail.com>
2016-10-30 22:04:21 -0500

    second commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,19 @@
 // Number of Questions
-var questNum = 5;
-
-// Answer Options
-var options = 4;
+var questionNum = 5;
 
 // Number of questions displayed in answer area
-var answers = new Array(5);
+var answers = [];
+
+// declaring questions
+
+var question1
+var question2
+var question3
+var question4
+var question5
+
+//placeholder for button values
+var userInput = [];
 
 // Answers
 answers[0] = "Wilt";
@@ -15,23 +23,28 @@ answers[3] = "Ben";
 answers[4] = "Kareem";
 
 // function to get score ...
-function getScore(form) {
+function getScore() {
   var score = 0;
-  var currElt;
-  var currSelection;
-  for (i=0; i<questNum; i++) {
-    currElt = i*options;
-    for (j=0; j<options; j++) {
-      currSelection = form.elements[currElt + j];
-      if (currSelection.checked) {
-        if (currSelection.value == answers[i]) {
-          score++;
-          break;
-        }
+  var questionNum = 5;
+  var form = document.getElementById('form1');
+    userInput[0] = form1.question1.value;
+    userInput[1] = form1.question2.value;
+    userInput[2] = form1.question3.value;
+    userInput[3] = form1.question4.value;
+    userInput[4] = form1.question5.value;
+
+
+
+  for (i=0; i<questionNum.length; i++) {
+
+    if (userInput[i] == answers[i]) {
+
+      if (userInput[i] == answers[i]) {
+          score += 1;
+      } else {
+          score += 0;
       }
     }
+    alert(score);
   }
-  form.solutions.value = correctAnswers;
-  ;
-}
-
+};
