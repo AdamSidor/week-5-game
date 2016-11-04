e09bf3355df9f182f2cfbc1a8a78b2b46076cc52
Adam Sidor <adamsidor94@gmail.com>
2016-11-04 18:04:14 -0500

    Another one

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,14 +4,9 @@ var questionNum = 5;
 
 // declaring questions
 
-var answers = [
-  'Wilt', 
-  'CLE' ,
-  'Adam' ,
-  'Ben' ,
-  'Kareem']
+var answers = [ 'Wilt', 'CLE' , 'Adam' , 'Ben' , 'Kareem'];
  
-
+var correctAnswers = 0;
 
 //settimeout
 //set some kind of thing to happen once time runs out
@@ -65,85 +60,40 @@ clearInterval(counter);
 
 
 
-
+var radioAll = [];
 
 //placeholder for button values
 function getAnswers() {
-
-var radioDude1 = $(".form1").val();  
-var radioDude2 = $(".form2").val();  
-var radioDude3 = $(".form3").val();  
-var radioDude4 = $(".form4").val();  
-var radioDude5 = $(".form5").val();  
-
-var userInput = [radioDude1, radioDude2 , radioDude3 , radioDude4 , radioDude5]
-
-
-console.log(radioDude1);
-console.log(radioDude2);
-console.log(radioDude3);
-console.log(radioDude4);
-console.log(radioDude5);
+  var radioDude1 = $('input[name="question1"]:checked').val(); 
+    radioAll.push(radioDude1);
+  var radioDude2 = $('input[name="question2"]:checked').val();
+    radioAll.push(radioDude2);  
+  var radioDude3 = $('input[name="question3"]:checked').val();  
+    radioAll.push(radioDude3);  
+  var radioDude4 = $('input[name="question4"]:checked').val(); 
+    radioAll.push(radioDude4);  
+  var radioDude5 = $('input[name="question5"]:checked').val();   
+    radioAll.push(radioDude5);  
 
 }
 
 
-
-// function to get values from buttons
-
-// function getRadioValue() {
-//     var radios = document.getElementById("");
-//     window.rdValue; 
-//         for (var i=0; i<questionNum.length; i++) {
-//              var radioVal = radios[i];
-//               if (radioVal.checked) {
-//                 rdValue = radioVal.value;
-//                     break;
-//                 }
-//             else rdValue = 'noRadioChecked';
-//             }
-//               if (rdValue == '5') {
-//               alert('5'); 
-//             }
-//             else if (rdValue == 'noRadioChecked') {
-//               alert('no radio checked');
-//             }
-//         }
-
-// var radios = getRadioValue();
-// // function to get score 
-// function getScore() {
-//   var score = 0;
-//   var questionNum = 5;
-//   var form = document.getElementById('form1');
-//     userInput[0] = form1.question1.radios;
-//     userInput[1] = form1.question2.radios;
-//     userInput[2] = form1.question3.radios;
-//     userInput[3] = form1.question4.radios;
-//     userInput[4] = form1.question5.radios;
+getAnswers();
 
 
+ function checkAnswer() {
+   for (var i=0; i<answers.length; i++) {
+     console.log(radioAll[i]);
+     console.log(answers[i]);
+     if (radioAll[i] == answers[i]){
+       correctAnswers ++;
+     }
+   }
+ };
 
-//   for (i=0; i<questionNum.length; i++) {
+ checkAnswer();
 
-//     if (userInput[i] == answers[i]) {
-
-//       if (userInput[i] == answers[i]) {
-//           score += 1;
-//       } else {
-//           score += 0;
-//       }
-//     }
-//     alert(score);
-//   }
-// };
-// getRadioValue();
-
-
-/*set up div for welcome screen 
-set up div for game over*/
-
-
+ $('#answerDiv').text( "You got "  + correctAnswers + " questions correct")
 
 
 
@@ -153,6 +103,6 @@ $(document).ready(function(){
       $("#SplashScreen").hide();
       $("#GameCanvas").show();
     });
-  $("#")
+  $("#scoreButton")
 
 });
